4134fb1f3766a61549ee580425f1c3df37fc1f99
Sinity <ezo.dev@gmail.com>
2022-08-02 06:04:15 +0200

    Commited stuff that was not commited after dropping the project

--- native_messaging_host/src/main.cpp
+++ native_messaging_host/src/main.cpp
@@ -1,9 +1,15 @@
 #include "native_messaging.h"
+#include "netsock/NetSock.h"
 
 int main()
 {
     NativeMessaging::init();
     NativeMessaging::sendMessage("Native messaging host started.");
+
+    NetSock::InitNetworking();
+    NetSock socket;
+
+    auto connected = socket.Connect("127.0.0.1", 50332);
 
     while(true) {
         auto response = NativeMessaging::readResponse();
@@ -11,7 +17,15 @@ int main()
             break;
         }
 
-        //TODO: send the response to the main app
+        //try to connect if we're not yet connected
+        if(!connected) {
+            connected = socket.Connect("127.0.0.1", 50332);
+        }
+
+        //if we're connected, send the data
+        if(connected) {
+            socket.Write(response.c_str(), response.size());
+        }
     }
 
     return 0;
